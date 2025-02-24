50e7fa9c36f6fd86db1d980ce8a228e43c2a9956
Rido Fuad <ridofuad@gmail.com>
2025-02-24 10:46:06 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius illum consequatur magnam nobis, nemo nulla quos sint sunt magni alias optio sit esse, quam minima fuga commodi ducimus sapiente. Eos.</p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
